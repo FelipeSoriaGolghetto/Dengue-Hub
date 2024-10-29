4552a0123a7746cbf554b6480b895d4d4d97b32b
Tiago Felice <tiagofelice@MacBook-Air-de-Tiago.local>
2024-10-29 14:32:25 -0300

    página de autenticação e integração com os componentes

---
+++ FrontEnd/dengue-hub/pages/authentication.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import Navbar from '../components/navbar';
+import Drawer from '../components/drawer';
+
+const Autenticacao = () => {
+  const solicitacoes = [
+    { id: 1, nome: 'Alice Santos', profissao: 'Professora de Biologia' },
+    { id: 2, nome: 'Carlos Oliveira', profissao: 'Estudante de Biomedicina' }
+  ];
+
+  const handleAccept = (id) => {
+    alert(`Solicitação de ID ${id} foi aceita.`);
+  };
+
+  const handleReject = (id) => {
+    alert(`Solicitação de ID ${id} foi rejeitada.`);
+  };
+
+  return (
+    <div className='layout' style={{ backgroundColor: '#FFFFFF', minHeight: '100vh', padding: '20px' }}>
+      <Navbar /> 
+      <Drawer /> 
+      <h1 style={{ fontSize: '34px', fontWeight: 'bold', marginTop: '30px', textAlign: 'center', marginBottom: '60px'}}>Autenticação de usuários</h1>
+      {solicitacoes.map((solicitacao) => (
+        <div key={solicitacao.id} style={styles.card}>
+          <div style={styles.infoContainer}>
+            <p style={styles.text}><strong>Nome:</strong> {solicitacao.nome}</p>
+            <p style={styles.text}><strong>Profissão:</strong> {solicitacao.profissao}</p>
+          </div>
+          <div style={styles.buttonContainer}>
+            <button onClick={() => handleAccept(solicitacao.id)} style={styles.acceptButton}>Aceitar</button>
+            <button onClick={() => handleReject(solicitacao.id)} style={styles.rejectButton}>Rejeitar</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const styles = {
+  card: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    border: '1px solid #ddd',
+    borderRadius: '8px',
+    padding: '15px',
+    marginBottom: '20px',
+    width: '100%',
+    maxWidth: '1200px',
+    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+    marginLeft: '60px',
+  },
+  infoContainer: {
+    flex: '1',
+  },
+  text: {
+    fontSize: '16px',
+    margin: '5px 0',
+  },
+  buttonContainer: {
+    display: 'flex',
+    gap: '10px',
+  },
+  acceptButton: {
+    backgroundColor: '#4CAF50',
+    color: 'white',
+    padding: '10px 20px',
+    border: 'none',
+    borderRadius: '20px',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+  },
+  rejectButton: {
+    backgroundColor: '#f44336',
+    color: 'white',
+    padding: '10px 20px',
+    border: 'none',
+    borderRadius: '20px',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+  },
+};
+
+export default Autenticacao;
