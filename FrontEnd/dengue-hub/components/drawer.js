4b9ba764710eabeb8504900fda1a5bee36aa6361
Andrew <andrewcsa@usp.br>
2024-12-03 03:11:27 -0300

    Add icones drawer

--- FrontEnd/dengue-hub/components/drawer.js
+++ FrontEnd/dengue-hub/components/drawer.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import '../styles/global.css';
 import Image from 'next/image';
 import Logo from '../public/logo-mosquito.svg';
+import Paper from '../public/list-paper-school-svgrepo-com.svg';
 import { getUserById } from '../services/users'; // Importando a função da API
 
 const Drawer = () => {
@@ -42,11 +43,12 @@ const Drawer = () => {
               <div className="divider divider-accent"></div>
             </div>
             <div  className="items-start" >
-              <li><a href='/'><img src="/logo-mosquito.svg" alt="Logo" width={40} height={40} className="mr-1" /> Wiki</a></li>
+              <li><a href='/todos'><img src="/logo-mosquito.svg" alt="Logo" width={40} height={40} className="mr-1" /> Wiki</a></li>
               <li><a href='/login'><img src="/user-icon.svg" alt="Login" width={40} height={40} className="mr-1" /> Login</a></li>
               <li> <a onClick={handleGetUser}> {/* Modifique o botão "Estatísticas" */}<img src="/file.svg" alt="Estatísticas" width={40} height={40} className="mr-1" />Estatísticas</a></li>
               <li><a><img src="/globe.svg" alt="Globe" width={40} height={40} className="mr-1" /> Mapas</a></li>
-              <li><a><img src="/window.svg" alt="Window" width={40} height={40} className="mr-1" /> Histórico</a></li>
+              <li><a href='/createArticle'><img src="/list-paper-school-svgrepo-com.svg" alt="Novo Artigo" width={40} height={40} className="mr-1" /> Novo Artigo</a></li>
+              <li><a href='/updateArticle'><img src="/pencil-svgrepo-com.svg" alt="Atualizar Artigo" width={40} height={40} className="mr-1" /> Atualizar Artigo</a></li>
               <li><a href='/admin'><img src="/user-check.svg" alt="Auth" width={40} height={40} className="mr-1" /> Autenticação de usuários</a></li>
             </div>
           </ul>
